fe0409e57f0fb6acb73d0fca0aa2439536fcf2dc
Justynr <justynr@me.com>
2021-04-27 10:51:29 -0400

    Colour List & cycling to next colour

--- Generator.jsx
+++ Generator.jsx
@@ -82,16 +82,22 @@ function letsGo() {
   const height = parseInt(box.panel2.group2.height.text)
   const column = parseInt(box.panel2.group2.column.text)
 
-  const xPosStart = 40
-  const yPosStart = 40
-  var xPos = xPosStart
 
-  var colorList = ['red', 'lime', 'cyan', 'magenta', 'blue']
+  var idoc = app.activeDocument;
+  var activeAB = idoc.artboards[idoc.artboards.getActiveArtboardIndex()]; // get active AB
+
+  docLeft = activeAB.artboardRect[0];
+  docTop = activeAB.artboardRect[1]; 
+
+  const xPosStart = docLeft + width/2
+  var yPosStart = docTop - width
+  var xPos = xPosStart
 
   var shapeOptions = {
     filled : true,
     stroked : false,
     strokeWidth : 0,
+    opacity: 75
   }
 
   if (box.panel.shape_star.value == true){ 
@@ -158,21 +164,24 @@ function letsGo() {
   if (box.panel.shape_square.value == true) {
     //app.defaultStroked = true;
     app.filled = true;
-
-
         
     for ( r = 1; r < (count+1); r++ ) {
+      var squareGroup = app.activeDocument.groupItems.add();
+      
       var row = (Math.ceil(r/column)-1) * -1
       var yPos = row * height + yPosStart
-      var squareGroup = app.activeDocument.groupItems.add();
       var rect = squareGroup.pathItems.rectangle( yPos, xPos, width, height );
-      var fillColor = new RGBColor
-      fillColor.red = getRandomNum(128,255);
-      fillColor.green = getRandomNum(128,255);
-      fillColor.blue = getRandomNum(128,255);
-      rect.fillColor = fillColor;
-      rect.stroked = shapeOptions.stroked;
+
       xPos = xPosStart + (width * (r%column));
+      
+      var fillColor = new RGBColor
+      var newColour = getNextColor(r)
+      fillColor.red = newColour[0];
+      fillColor.green = newColour[1];
+      fillColor.blue = newColour[2];
+      rect.fillColor = fillColor;
+      rect.opacity = shapeOptions.opacity;
+      rect.stroked = shapeOptions.stroked;
     }
   }
 
@@ -180,26 +189,26 @@ function letsGo() {
   if (box.panel.shape_hexagon.value == true) {
 
     width = width*2
-
     var hexHigh = ((Math.sqrt(3)/2) * width)
     var xOffset = width * 0.75
         
     for ( r = 0; r < (count); r++ ) {
+      var hexGroup = app.activeDocument.groupItems.add();
+      
       var row = (Math.ceil((r+1)/column)-1) * -1
       var yOffset = (hexHigh * ((r)%2))/2
-      var yPos = ((row * hexHigh) + yPosStart) + yOffset
-      var hexGroup = app.activeDocument.groupItems.add();
-
-      xPos = ((( r % (column) ) ) * xOffset) + (( xOffset * (( row * -1 ) % 2 ) * ( column % 2 ))) + (xPosStart * 2)
+      var yPos = ( ( row * hexHigh ) + ( yPosStart * 2 ) ) + yOffset
+      xPos = ((( r % (column) ) ) * xOffset) + (( xOffset * (( row * -1 ) % 2 ) * ( column % 2 ))) + (xPosStart * 2.25)
 
       var hexagon = hexGroup.pathItems.polygon( xPos, yPos, width/2, 6 );
+      
       var fillColor = new RGBColor
-
-      fillColor.red = getRandomNum(64,255);
-      fillColor.green = getRandomNum(64,255);
-      fillColor.blue = getRandomNum(64,255);
+      var newColour = getNextColor(r)
+      fillColor.red = newColour[0];
+      fillColor.green = newColour[1];
+      fillColor.blue = newColour[2];
       hexagon.fillColor = fillColor
-      hexagon.opacity = 50;
+      hexagon.opacity = shapeOptions.opacity;
       hexagon.stroked = shapeOptions.stroked;
     }
   }
@@ -210,27 +219,57 @@ function letsGo() {
     var xOffset = width * 0.5
 
     var triangleSide = width * 0.5773503161570331
+
+    var rndColStart = getRandomNum(0,100)
         
     for ( r = 0; r < (count); r++ ) {
+      var triangleGroup = app.activeDocument.groupItems.add();
+      
       var row = (Math.ceil((r+1)/column)-1) * -1
       var yOffset = (hexHigh * ((r)%2))/2
       var yPos = ((row * hexHigh/2) + yPosStart) 
-      var triangleGroup = app.activeDocument.groupItems.add();
-
       xPos = ((( r % column ) ) * xOffset ) + (( (xOffset * -1) * (( row * -1 ) % 2 ) * ( column % 2 ))) + ( xPosStart * 2 )
 
       var triangle = triangleGroup.pathItems.polygon( xPos, yPos, triangleSide, 3 );
       var fillColor = new RGBColor
 
-      fillColor.red = getRandomNum(64,255);
-      fillColor.green = getRandomNum(64,255);
-      fillColor.blue = getRandomNum(64,255);
+      var newColour = getNextColor(r)
+      fillColor.red = newColour[0];
+      fillColor.green = newColour[1];
+      fillColor.blue = newColour[2];
       triangle.fillColor = fillColor
-      triangle.opacity = 50;
+      triangle.opacity = shapeOptions.opacity;
       triangle.stroked = shapeOptions.stroked;
       triangle.rotate(180 * ((r % 2) + (row % 2)))
     }
   }
+}
+
+var colorList = [
+  [255,0,0], 
+  [39,213,7], 
+  [0,255,255], 
+  [255,0,255], 
+  [0,0,255], 
+  [166,152,246], 
+  [143,21,0], 
+  [7,91,226], 
+  [239,99,2],
+  [117,243,54],
+  [117,	54,	243],
+  [153,153,153],
+  [75,75,75]
+]
+
+function getNextColor(i){
+  var col = i % colorList.length
+  return (colorList[col])
+}
+
+//Get Random Colour
+function getRandomColor(){
+  const rnd = Math.round(getRandomNum(0, colorList.length - 1),0)
+  return(colorList[rnd])
 }
 
 // generating random number in range [x, y)
